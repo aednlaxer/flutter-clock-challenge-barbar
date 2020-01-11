94efc8c513ae0420a5bd5804a9cb2e08b5725381
Alexander <20989940+aednlaxer@users.noreply.github.com>
2020-01-11 11:32:28 +0200

    Move painter and CustomPaint to separate file

--- digital_clock/lib/digital_clock.dart
+++ digital_clock/lib/digital_clock.dart
@@ -4,8 +4,7 @@
 
 import 'dart:async';
 
-import 'package:digital_clock/data/digits_collection.dart';
-import 'package:digital_clock/model/display_bar.dart';
+import 'package:digital_clock/widget/clock_face.dart';
 import 'package:flutter_clock_helper/model.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -80,18 +79,18 @@ class _DigitalClockState extends State<DigitalClock> {
       _dateTime = DateTime.now();
       // Update once per minute. If you want to update every second, use the
       // following code.
-      _timer = Timer(
-        Duration(minutes: 1) -
-            Duration(seconds: _dateTime.second) -
-            Duration(milliseconds: _dateTime.millisecond),
-        _updateTime,
-      );
+//      _timer = Timer(
+//        Duration(minutes: 1) -
+//            Duration(seconds: _dateTime.second) -
+//            Duration(milliseconds: _dateTime.millisecond),
+//        _updateTime,
+//      );
       // Update once per second, but make sure to do it at the beginning of each
       // new second, so that the clock is accurate.
-      // _timer = Timer(
-      //   Duration(seconds: 1) - Duration(milliseconds: _dateTime.millisecond),
-      //   _updateTime,
-      // );
+      _timer = Timer(
+        Duration(seconds: 1) - Duration(milliseconds: _dateTime.millisecond),
+        _updateTime,
+      );
     });
   }
 
@@ -102,118 +101,16 @@ class _DigitalClockState extends State<DigitalClock> {
         : _darkTheme;
     final hour =
         DateFormat(widget.model.is24HourFormat ? 'HH' : 'hh').format(_dateTime);
-    final minute = DateFormat('mm').format(_dateTime);
-    final fontSize = MediaQuery.of(context).size.width / 3.5;
-    final offset = -fontSize / 7;
-    final defaultStyle = TextStyle(
-      color: colors[_Element.text],
-      shadows: [
-        Shadow(
-          blurRadius: 0,
-          color: colors[_Element.shadow],
-          offset: Offset(10, 0),
-        ),
-      ],
-    );
+    final minute = DateFormat('ss').format(_dateTime); // FIXME mm
 
     return Container(
       color: colors[_Element.background],
       child: Center(
-        child: CustomPaint(
-          size: Size.infinite,
-          painter: MyPainter(),
+        child: ClockFace(
+          displayedHours: hour,
+          displayedMinutes: minute,
         ),
-//        child: DefaultTextStyle(
-//          style: defaultStyle,
-//          child: Stack(
-//            children: <Widget>[
-//              Positioned(left: offset, top: 0, child: Text(hour)),
-//              Positioned(right: offset, bottom: offset, child: Text(minute)),
-//            ],
-//          ),
-//        ),
       ),
     );
-  }
-}
-
-class MyPainter extends CustomPainter {
-  static const int _DIGIT_BAR_COUNT = 6;
-  static const int _DIGIT_SEPARATOR_BAR_COUNT = 1;
-  static const int _COLON_BAR_COUNT = 2;
-  static const int _PADDING_BAR_COUNT = 4;
-
-  // How much bar takes of available bar+space width
-  static const double _BAR_COEFFICIENT = 1 / 3;
-
-  // How much space takes of available bar+space width
-  static const double _SPACE_COEFFICIENT = 2 / 3;
-
-  // Calculate requested bar count
-  static const int requestedBarCount = _PADDING_BAR_COUNT +
-      _DIGIT_BAR_COUNT + // first hour digit
-      _DIGIT_SEPARATOR_BAR_COUNT +
-      _DIGIT_BAR_COUNT + // second hour digit
-      _DIGIT_SEPARATOR_BAR_COUNT +
-      _COLON_BAR_COUNT + // colon
-      _DIGIT_SEPARATOR_BAR_COUNT +
-      _DIGIT_BAR_COUNT + // first minute digit
-      _DIGIT_SEPARATOR_BAR_COUNT +
-      _DIGIT_BAR_COUNT + // second minute digit
-      _PADDING_BAR_COUNT;
-
-  final _digitsCollection = DigitsCollection();
-
-  void showTime() {}
-
-  void _diff(old, current, progress) {}
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final digit = _digitsCollection.getDigit(2);
-
-    final double availableBarWidth =
-        size.width / (requestedBarCount + _SPACE_COEFFICIENT);
-    final double spaceWidth = availableBarWidth * _SPACE_COEFFICIENT;
-    final double barWidth = availableBarWidth * _BAR_COEFFICIENT;
-    print(
-        "$requestedBarCount width=${size.width} $availableBarWidth $barWidth $spaceWidth");
-
-    // TODO move
-    final barPaint = Paint()
-      ..color = Colors.black
-      ..strokeWidth = 0
-      ..style = PaintingStyle.fill;
-
-    for (var bar = 0; bar < requestedBarCount; bar++) {
-      final double barXStart = spaceWidth + bar * barWidth + bar * spaceWidth;
-      final double barXEnd = barXStart + barWidth;
-
-      final where = digit.where((item) {
-        return item.barNumber == bar;
-      });
-      if (where.isNotEmpty) {
-        where.forEach((item) {
-          final barTop = item.startY * size.height;
-          final barBottom = item.endY * size.height;
-          final rect = Rect.fromLTRB(barXStart, barTop, barXEnd, barBottom);
-          print("$bar not empty $barTop $barBottom");
-          canvas.drawRect(rect, barPaint);
-        });
-      } else {
-        final rect = Rect.fromLTRB(barXStart, .0, barXEnd, size.height);
-        canvas.drawRect(rect, barPaint);
-      }
-
-      // todo check bounds when drawing
-    }
-  }
-
-  // TODO accessibility
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    // TODO: implement shouldRepaint
-    return true;
   }
 }
